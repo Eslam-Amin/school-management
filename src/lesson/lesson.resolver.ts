60afdf7805a13deae88c0ec8063d33d135355467
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-02 01:23:33 +0200

    Used Create lesson Input to the service, and the resolver

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
@@ -11,12 +12,8 @@ export class LessonResolver {
   }
 
   @Mutation((returns) => LessonType)
-  createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
-  ) {
-    const lesson = this.lessonService.createLesson(name, startDate, endDate);
+  createLesson(@Args('body') body: CreateLessonInput) {
+    const lesson = this.lessonService.createLesson(body);
     return lesson;
   }
 
--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -10,7 +11,8 @@ export class LessonService {
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
 
-  createLesson(name, startDate, endDate): Promise<Lesson> {
+  createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
