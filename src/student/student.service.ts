821f05a793ec230b81dfd226696ec80d86d3bfb8
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-02 16:16:27 +0200

    Added Student service function

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Student } from './student.entity';
+import { CreateStudentInput } from './create-student.input';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = await this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    return this.studentRepository.save(student);
+  }
+
+  async findAll(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
+  async findOne(id: string): Promise<Student> {
+    const student = await this.studentRepository.findOneBy({ id });
+    if (!student) throw new NotFoundException('Student not found');
+    return student;
+  }
+}
